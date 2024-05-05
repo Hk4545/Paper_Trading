8910f71e449a747b0a90e58e37339052579e0570
HariKrishnaAllu4545 <harikrishna@opstronomy.com>
2024-05-05 19:18:23 +0530

    second commit

--- paper_trading_backend/src/app.module.ts
+++ paper_trading_backend/src/app.module.ts
@@ -6,6 +6,8 @@ import { SignupModule } from './signup/signup.module';
 import { LoginModule } from './login/login.module';
 import { JwtModule } from '@nestjs/jwt';
 import { OrderController } from './order/order.controller';
+import * as dotenv from 'dotenv';
+dotenv.config();
 // import { Order } from './order/order';
 import { OrderService } from './order/order.service';
 import { OrderModule } from './order/order.module';
@@ -13,7 +15,7 @@ import { StockinfoModule } from './stockinfo/stockinfo.module';
 import * as crypto from 'crypto';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://alluharikrishnachowdary45:VUaW3XnjtlfvFsX9@tradingapp.eb4joq4.mongodb.net/'), SignupModule, LoginModule
+  imports: [MongooseModule.forRoot(process.env.MONGO), SignupModule, LoginModule
     , JwtModule.registerAsync({
       useFactory: () => ({
           secret: crypto.randomBytes(32).toString('hex'),
